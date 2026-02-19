817fcfc4f5432dd8a0de0fefc5b6d64241b597f3
Narciso Oliveira <nrcsfo@gmail.com>
2026-02-19 17:04:58 -0300

    Criação do projeto Spring Boot. Configuração. Estruturação das camadas. Criação dos arquivos base. Projeto preparado para implementação das entidades e regras de negócio.

---
+++ src/main/java/com/example/desafio_tecnico_sergipetec/DesafioTecnicoSergipetecApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_sergipetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoSergipetecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTecnicoSergipetecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desafio_tecnico_sergipetec/DesafioTecnicoSergipetecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_sergipetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoSergipetecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
